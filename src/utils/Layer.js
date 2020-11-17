05ca1659d164efe60a2a2d912891db80e0e34c99
Eric <eric.lopezs@alumnos.udg.mx>
2020-11-16 18:50:57 -0600

    Remove standalone training functionality from neurons

--- src/utils/Layer.js
+++ src/utils/Layer.js
@@ -4,44 +4,12 @@ function Layer(descriptor) {
   let layer = {
     neurons: Array.from(Array(descriptor)).map(Neuron),
     error: Infinity,
+    predict,
   };
-
-  layer.addTrainingData = function (inputs, outputs) {
-    layer.neurons = outputs.map((outputColumn) => {
-      let neuron = Neuron();
-      inputs.forEach((inputRow, i) =>
-        neuron.training.addRule({ inputs: inputRow, target: outputColumn[i] })
-      );
-      return neuron;
-    });
-  };
-
-  layer.categorize = function (input) {
-    return layer.neurons.reduce(
-      (acc, neuron) => acc + Math.round(neuron.predict(input)),
-      ""
-    );
-  };
-
-  layer.converges = function (acceptableError = 0.0001) {
-    return layer.error < acceptableError;
-  };
-
-  layer.train = function () {
-    const actualOutputs = [];
-    let _error = 0;
-    layer.neurons.forEach((neuron) => {
-      neuron.training.train();
-      _error += Math.pow(neuron.training.error(), 2);
-      actualOutputs.push(neuron.training.currentPredictions());
-    });
-    layer.error = _error / layer.neurons.length;
-    return actualOutputs;
-  };
-
-  layer.predict = function (inputs) {
-    return layer.neurons.map((neuron) => neuron.predict(inputs));
-  };
+  function predict(inputs) {
+    layer.output = layer.neurons.map((neuron) => neuron.predict(inputs));
+    return layer.output;
+  }
   return layer;
 }
 
--- src/utils/Neuron.js
+++ src/utils/Neuron.js
@@ -1,21 +1,6 @@
-function dot(v1, v2) {
-  let result = 0;
-  for (let i = 0; i < v1.length; i++) {
-    result += v1[i] * v2[i];
-  }
-  return result;
-}
+import { dot, sigmoidActivation } from "./math";
 
-function stepActivation(output) {
-  return output > 0 ? 1 : 0;
-}
-
-function sigmoidActivation(output) {
-  const ex = Math.exp(output);
-  return ex / (ex + 1);
-}
-
-function Neuron(inputQuantity = 2, step = 0.5) {
+function Neuron(inputQuantity = 2) {
   let [weights, bias] = [
     Array.from(Array(inputQuantity)).map((_) => Math.random()),
     Math.random(),
@@ -26,13 +11,8 @@ function Neuron(inputQuantity = 2, step = 0.5) {
     bias,
     predict,
     adjust,
-    training: {
-      rules: [],
-      addRule,
-      train,
-      error,
-      currentPredictions,
-    },
+    inputs: null,
+    output: null,
   };
 
   function _predict(inputs) {
@@ -42,58 +22,20 @@ function Neuron(inputQuantity = 2, step = 0.5) {
   function predict(inputs) {
     while (inputs.length > neuron.weights.length)
       neuron.weights.push(Math.random());
+    neuron.inputs = inputs;
+    neuron.output = sigmoidActivation(_predict(inputs));
 
-    return sigmoidActivation(_predict(inputs));
+    return neuron.output;
   }
 
-  function adjust(inputs, error) {
-    let diff = step * error;
-    neuron.bias -= diff;
+  function adjust(inputs, delta) {
+    neuron.bias -= delta;
     for (let i = 0; i < neuron.weights.length; i++) {
-      neuron.weights[i] += diff * inputs[i];
+      neuron.weights[i] += delta * inputs[i];
     }
   }
 
-  function currentPredictions() {
-    return neuron.training.rules.map((rule) => neuron.predict(rule.inputs));
-  }
-
-  function error() {
-    let accum = 0;
-    neuron.training.rules.forEach((rule) => {
-      let target = rule.target;
-      let actual = neuron.predict(rule.inputs);
-      accum += Math.pow(target - actual, 2);
-    });
-    return accum / neuron.training.rules.length;
-  }
-
-  function addRule(rule) {
-    neuron.training.rules.push(rule);
-  }
-
-  function train() {
-    neuron.training.rules.forEach((rule) => {
-      let target = rule.target;
-      let actual = neuron.predict(rule.inputs);
-      let error = target - actual;
-      if (target != actual) adjust(rule.inputs, error);
-    });
-  }
-
   return neuron;
-}
-
-function generateLine(neuron) {
-  const [w1, w2] = neuron.weights;
-  const bias = neuron.bias;
-  // y = b/w2 - w1x1/w2
-  let leftLimit = -2,
-    rightLimit = 3;
-
-  let p1 = { x: leftLimit, y: bias / w2 - (w1 * leftLimit) / w2 };
-  let p2 = { x: rightLimit, y: bias / w2 - (w1 * rightLimit) / w2 };
-  return [p1, p2];
 }
 
 export { Neuron };
